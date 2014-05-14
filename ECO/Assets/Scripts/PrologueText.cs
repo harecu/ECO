6e2c5fb7a2fc59c97f2ac801c4ee92781eb53513
Kurt Lippert <klipper@ncsu.edu>
2014-05-14 00:58:20 -0400

    Created a new scene for testing GUI stuff. Added a bunch of NGUI labels and widgets and things for testing.

--- ECO/Assets/Scripts/PrologueText.cs
+++ ECO/Assets/Scripts/PrologueText.cs
@@ -1,7 +1,28 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class PrologueText : MonoBehaviour
 {
+
+    public UILabel label;
+
+    void Start()
+    {                
+        string[] words = label.text.Split(null);
+
+        StartCoroutine(TextFadeIn());        
+    }
+
+    IEnumerator TextFadeIn()
+    {
+
+        yield return 0;
+    }
+
+    void Update()
+    {
+        
+    }
 
     void OnGUI()
     {
